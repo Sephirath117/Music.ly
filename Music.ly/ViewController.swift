13dbab5c59a400381006be43f7dda9023e0f3c9f
Karan Mehta <appllefanetickz@gmail.com>
2015-02-27 13:47:22 -0800

    Initial Commit

---
+++ Music.ly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music.ly
+//
+//  Created by Karan Mehta on 2/27/15.
+//  Copyright (c) 2015 Karan Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
